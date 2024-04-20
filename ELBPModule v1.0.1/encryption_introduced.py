1558e328ec9795609c64df966511880ab6bfb5c4
岩墨 <146601243+17750297502@users.noreply.github.com>
2024-04-20 15:46:49 +0800

    第二次更新
    加入了:
    到文本()
    到数值()
    到整数()
    到字节集()

---
+++ ELBPModule v1.0.1/encryption_introduced.py
@@ -0,0 +1,5 @@
+import  Syntax_python_Module
+from Syntax_python_Module import *
+
+#my daijunning,yanmo,f
+#The whole thing is developed and produced by daijunning,f,yanmo and is prohibited from plagiarizing and publishing.
